d2d7d0dc084d2e5bd8aae50d788ff0a1b9662e95
lufranski <lufranski@gmail.com>
2022-11-10 15:06:02 +0100

    innerHtml Output - Dadi

--- js-dadi/js/main.js
+++ js-dadi/js/main.js
@@ -1,28 +1,40 @@
 // Gioco dei dadi
 
 // Variables
-const userNumber = Math.floor(Math.random() * 6) + 1;
-const pcNumber = Math.floor(Math.random() * 6) + 1;
-
-console.log("Il numero dell'utente è: " , userNumber);
-console.log("Il numero del pc è: " , pcNumber);
-
+const userNumber = document.getElementById('user-number');
+const pcNumber = document.getElementById('pc-number');
+const whoWins = document.getElementById('result');
 
 // Generare un numero random da 1 a 6 per l'utente
+const userRandomNumber = Math.floor(Math.random() * 6) + 1;
+
+userNumber.innerHTML = userRandomNumber;
+
+console.log("Il numero dell'utente è: " , userRandomNumber);
 
 // Generare un numero random da 1 a 6 per il pc
+const pcRandomNumber = Math.floor(Math.random() * 6) + 1;
+
+pcNumber.innerHTML = pcRandomNumber;
+
+console.log("Il numero del pc è: " , pcRandomNumber);
 
 // Stabilire il vincitore in base al risultato
-if (userNumber < pcNumber) {
+if (userRandomNumber < pcRandomNumber) { //Vince il PC
 
-    console.log('Gameover, honey!');
+    // console.log('Gameover, honey!');
+    whoWins.innerHTML = 'Gameover, honey!';
 
-} else if (userNumber > pcNumber) {
 
-    console.log('A knockout!');
+} else if (userRandomNumber > pcRandomNumber) { //Vince l'utente
 
-} else {
+    // console.log('A knockout!');
+    whoWins.innerHTML = 'A Knockout!';
 
-    console.log("It's a draw!");
+
+} else { //Pareggio
+
+    // console.log("It's a draw!");
+    whoWins.innerHTML = "It's a draw!";
 
 }
